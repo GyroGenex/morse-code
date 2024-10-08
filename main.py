bef64831a2e9ddf1fb4a56e026e6da2ac5cda00e
gyrogenex <yektengcheh@gmail.com>
2024-10-08 11:25:14 +0800

    project complete

---
+++ main.py
@@ -0,0 +1,30 @@
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
+
+
+def converter(message):
+    morse_code = ""
+    for character in message:
+        morse_code += MORSE_CODE_DICT[character.upper()]
+        morse_code += " "
+
+    return morse_code
+
+
+message = input("Please enter the code you want to encrypt: ")
+converted_message = converter(message)
+
+print("Your converted message is " + converted_message)
